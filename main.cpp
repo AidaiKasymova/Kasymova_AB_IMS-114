2204ee2325cd63181d713622ae188465b2376989
Aidai Kasymova <80968071+AidaiKasymova@users.noreply.github.com>
2021-10-11 09:04:01 +0300

    Новые файлы работ.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
-#include "triad.h"
-#include "date.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "Date.h"
+#include "Time.h"
 
-using namespace std;
+int main()
+{
+	system("chcp 1251");
+	system("cls");
 
-int _tmain(int argc, _TCHAR* argv[]) {
-	triad p;
-	date day;
-	
-	day.set_first(5);
-	day.set_second(10);
-	day.set_third(15);
+	cout << "Тройка чисел." << endl;
+	Triad tr = Triad(3, 5, -4);
+	tr.output(cout);
 
-	p = day;
+	cout << endl << "Пусть x = 1, y = 2, z = -3." << endl;
+	tr.setX(1); tr.setY(2); tr.setZ(-3);
+	cout << tr.getX() << ", " << tr.getY() << ", " << tr.getZ() << endl;
 
-	cout << p << endl;
-	cout << day << endl;
+	cout << endl << "Зададим x, y, z." << endl;
+	tr.input(cin);
 
-	p.add_first(6);
-	p.add_second(12);
-	p.add_third(18);
+	cout << endl << "Увеличим на 1." << endl;
+	tr.add(); tr.output(cout);
 
-	cout << p << endl;
-	day.set_day(7);
-	cout << day << endl;
+	cout << endl << "Уменьшим на 1." << endl;
+	tr.sub(); tr.output(cout);
 
-	cout << day.get_newDay() << endl;
-	cin >> p;
+	cout << endl << "\nДата." << endl;
+	Date dt = Date(30, 9, 2021);
+	dt.output(cout);
+
+	cout << endl; dt.input(cin);
+	dt.output(cout);
+
+	cout << endl << "Увеличим дату на n дней." << endl;
+	dt.add(); dt.output(cout);
+
+	cout << endl << "Уменьшим дату на n дней." << endl;
+	dt.sub(); dt.output(cout);
+
+	tr = dt;
+	cout << endl << "Принцип подстановки: тройка чисел := дата. Тройка чисел: " << endl;
+	tr.output(cout);
+
+	system("pause");
+
 	return 0;
-
 }
